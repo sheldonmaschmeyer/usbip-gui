5a13ff919466e8a9772348b92687c5c70e7d0502
Sheldon Maschmeyer <sheldon@maschmeyer.ca>
2026-07-11 23:28:35 -0400

    Bump version to 1.2.0 and references in about

--- usbip_gui/gui/menu/about.py
+++ usbip_gui/gui/menu/about.py
@@ -1,7 +1,15 @@
 """About dialog and application information."""
 
-from PyQt6.QtWidgets import QMessageBox, QWidget
+from PyQt6.QtWidgets import (
+    QDialog,
+    QVBoxLayout,
+    QLabel,
+    QDialogButtonBox,
+    QWidget,
+)
+from PyQt6.QtCore import Qt
 from ..common import VERSION, get_translator
+from ...typings import connect_signal
 
 t = get_translator("menu")
 
@@ -12,9 +20,25 @@ def show_about_dialog(parent: QWidget | None = None):
         f"{t('Version')} {VERSION}\n\n"
         f"{t('Original Author')}: K-Francis-H\n"
         f"{t('Fork Maintainer')}: Sheldon Maschmeyer\n\n"
+        f"{t('Powered by PyQt6 and USB/IP')}\n"
+        f"PyQt6: https://doc.qt.io/qtforpython-6/licenses.html\n"
+        f"USB/IP: https://github.com/torvalds/linux/blob/master/"
+        f"tools/usb/usbip/README\n\n"
         f"GitHub: https://github.com/sheldonmaschmeyer/usbip-gui"
     )
 
-    QMessageBox.about(
-        parent, t("About") + " " + t("USB/IP Manager"), about_text
-    )
+    dialog = QDialog(parent)
+    dialog.setWindowTitle(t("About") + " " + t("USB/IP Manager"))
+
+    layout = QVBoxLayout(dialog)
+    layout.setContentsMargins(20, 20, 20, 20)
+
+    label = QLabel(about_text)
+    label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+    layout.addWidget(label)
+
+    button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+    connect_signal(button_box.accepted, dialog.accept)
+    layout.addWidget(button_box)
+
+    dialog.exec()
